9fb8b9cc9688a83241eea4a1f6fba8caa4532122
Daniil1D <rakincevdaniil18@gmail.com>
2023-06-14 08:56:16 +0300

    q

--- glavnai.php
+++ glavnai.php
@@ -10,9 +10,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $insertQuery = "INSERT INTO `Todo List` (`id`,`text`, `Statusname`, `active_from`, `active_to`) VALUES (NULL,'$todo','','','')";
     mysqli_query($link, $insertQuery);
 
-     // Перенаправление на текущую страницу для обновления.
-     header('Location: ' . $_SERVER['PHP_SELF']);
-     exit;
+    // Перенаправление на текущую страницу для обновления.
+    header('Location: ' . $_SERVER['PHP_SELF']);
+    exit;
+  }
+}
+
+// Обработка выделения записей
+$highlightedTodos = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['highlight'])) {
+  $highlightedTodos = $_GET['highlight'];
+
+  if (!is_array($highlightedTodos)) {
+    $highlightedTodos = [$highlightedTodos];
   }
 }
 
@@ -23,6 +34,7 @@ $result = mysqli_query($link, $selectQuery);
 
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -31,6 +43,7 @@ $result = mysqli_query($link, $selectQuery);
   <link rel="stylesheet" href="style.css">
   <title>Todo App</title>
 </head>
+
 <body>
   <div id="root" role="main" class="container">
     <div class="todo-app">
@@ -49,27 +62,58 @@ $result = mysqli_query($link, $selectQuery);
 
       <table class="table">
         <tbody>
-          <?php
-          while ($row = mysqli_fetch_assoc($result)) {
+          <?php while ($row = mysqli_fetch_assoc($result)):
             $text = $row['text'];
-            echo "<tr>";
-            echo "<td>$text</td>";
-            echo '<td>
-                    <button type="button" class="btn btn-outline-success btn-sm float-right">
-                      <i class="fa fa-exclamation"></i>
-                    </button>
-                    <button type="button" class="btn btn-outline-danger btn-sm float-right">
-                      <i class="fa fa-trash-o"></i>
-                    </button>
-                  </td>';
-            echo "</tr>";
-          }
+            $todoId = $row['id'];
           ?>
+            <tr>
+              <td>
+                <?php
+                // Проверяем, совпадает ли текущий todo с выделенными
+                if (in_array($todoId, $highlightedTodos)) {
+                  echo '<span class="todo-list-item-label" style="color: steelblue; font-weight: bold">';
+                } else {
+                  echo '<span class="todo-list-item-label">';
+                }
+                ?>
+                <?php echo $text; ?>
+                </span>
+              </td>
+              <td>
+                <form method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                  <?php
+                  // Создаем новый массив, содержащий только уникальные значения
+                  $newHighlightedTodos = array_unique($highlightedTodos);
+
+                  // Проверяем, совпадает ли текущий todo с выделенными
+                  if (in_array($todoId, $highlightedTodos)) {
+                    // Убираем текущий todo из выделенных
+                    $newHighlightedTodos = array_diff($highlightedTodos, [$todoId]);
+                  } else {
+                    // Добавляем текущий todo в выделенные
+                    $newHighlightedTodos[] = $todoId;
+                  }
+
+                  // Генерируем скрытые поля с массивом выделенных записей
+                  foreach ($newHighlightedTodos as $highlightedTodo) {
+                    echo '<input type="hidden" name="highlight[]" value="' . $highlightedTodo . '">';
+                  }
+                  ?>
+                  <button type="submit" class="btn btn-outline-success btn-sm float-right">
+                    <i class="fa fa-exclamation"></i>
+                  </button>
+                </form>
+                <button type="button" class="btn btn-outline-danger btn-sm float-right">
+                  <i class="fa fa-trash-o"></i>
+                </button>
+              </td>
+            </tr>
+          <?php endwhile; ?>
         </tbody>
       </table>
       <form class="bottom-panel d-flex" method="POST">
-          <input type="text" class="form-control new-todo-label" placeholder="What needs to be done?" name="todo" required>
-          <button type="submit" class="btn btn-outline-secondary" name="addTodo">Add</button>
+        <input type="text" class="form-control new-todo-label" placeholder="What needs to be done?" name="todo" required>
+        <button type="submit" class="btn btn-outline-secondary" name="addTodo">Add</button>
       </form>
     </div>
   </div>
