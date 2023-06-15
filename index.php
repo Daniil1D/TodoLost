c9fd860ebc657c5dc6e3bfdcc6921688a741b928
Daniil1D <rakincevdaniil18@gmail.com>
2023-06-15 09:57:45 +0300

    dqwd

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
 $link = mysqli_connect('localhost', 'root', '', 'Todo List');
 
+
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
   if (isset($_POST['addTodo'])) {
     $todo = $_POST['todo'];
 
     // SQL-запрос для вставки записи
-    $insertQuery = "INSERT INTO `Todo List` (`id`,`text`, `Statusname`, `active_from`, `active_to`) VALUES (NULL,'$todo','','','')";
+    $insertQuery = "INSERT INTO `Todo List` (`id`,`text`, `Statusname`, `active_from`, `active_to`) VALUES (NULL,'$todo','Active','','')";
     mysqli_query($link, $insertQuery);
 
     // Перенаправление на текущую страницу для обновления.
-    header('Location: ' . $_SERVER['PHP_SELF']);
+    header('Location: index.php');
     exit;
   }
 }
@@ -27,9 +29,46 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['highlight'])) {
   }
 }
 
+// Обработка GET-запроса на изменение статуса "Done"
+if (isset($_GET['done'])) {
+  $completeTodoId = $_GET['done'];
+
+  // Подготовка и выполнение запроса на обновление записи в базе данных
+  $query = "UPDATE `Todo List` SET `Statusname` = 'Done' WHERE `id` = $completeTodoId";
+
+  mysqli_query($link, $query);
+
+  // Перенаправление на текущую страницу для обновления
+  header('Location: ' . $_SERVER['PHP_SELF']);
+  exit;
+}
+
+// Обработка GET-запроса на изменение статуса "Active"
+if (isset($_GET['Active'])) {
+  $undoneTodoId = $_GET['Active'];
+
+  // Подготовка и выполнение запроса на обновление записи в базе данных
+  $query = "UPDATE `Todo List` SET `Statusname` = 'Active' WHERE `id` = $undoneTodoId";
+  mysqli_query($link, $query);
+
+  // Перенаправление на текущую страницу для обновления
+  header('Location: ' . $_SERVER['PHP_SELF']);
+  exit;
+}
+
+
 // Обновление результатов запроса после добавления записи
 $selectQuery = "SELECT * FROM `Todo List`";
 $result = mysqli_query($link, $selectQuery);
+
+
+// Получение выделенных записей с статусом "Done"
+$highlightedTodos = array();
+while ($row = mysqli_fetch_assoc($result)) {
+  if ($row['Statusname'] === 'Done') {
+    $highlightedTodos[] = $row['id'];
+  }
+}
 ?>
 
 <!DOCTYPE html>
@@ -54,55 +93,52 @@ $result = mysqli_query($link, $selectQuery);
       <div class="top-panel d-flex">
         <input type="text" class="form-control search-input" placeholder="type to search" />
         <div class="btn-group">
-          <button type="button" class="btn btn-info">All</button>
-          <button type="button" class="btn btn-outline-secondary">Active</button>
-          <button type="button" class="btn btn-outline-secondary">Done</button>
+          <a href="#" class="btn btn-info">All</a>
+          <a href="#" class="btn btn-outline-secondary">Active</a>
+          <a href="#" class="btn btn-outline-secondary">Done</a>
         </div>
       </div>
 
       <table class="table">
         <tbody>
-          <?php while ($row = mysqli_fetch_assoc($result)):
-            $text = $row['text'];
+          <?php if (isset($_GET['tab'])) {
+            $tab = $_GET['tab'];
+            if ($tab === 'Active') {
+              $selectQuery = "SELECT * FROM `Todo List` WHERE `Statusname` = 'Active'";
+            } elseif ($tab === 'done') {
+              $selectQuery = "SELECT * FROM `Todo List` WHERE `Statusname` = 'Done'";
+            } else {
+              $selectQuery = "SELECT * FROM `Todo List`"; //Если параметр tab не существует в GET-запросе, то выбирает все записи из таблицы "Todo List" без фильтрации по статусу.
+            }
+          } else {
+            $selectQuery = "SELECT * FROM `Todo List`";
+          }
+          $result = mysqli_query($link, $selectQuery);
+
+          while ($row = mysqli_fetch_assoc($result)) :
             $id = $row['id'];
+            $text = $row['text'];
+            $Statusname = $row['Statusname'];
           ?>
             <tr>
               <td>
-              <?php if (in_array($id, $highlightedTodos)): ?>
-                <span class="todo-list-item-label" style="color: steelblue; font-weight: bold">
-              <?php else: ?>
-                <span class="todo-list-item-label">
-              <?php endif; ?>
-                <?php echo $text; ?>
-                </span>
-              </td>
-              <td>
-                
-              <form method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-              <?php
-                // Создаем новый массив, содержащий только уникальные значения
-                $newHighlightedTodos = array_unique($highlightedTodos);
-
-                // Проверяем, совпадает ли текущий todo с выделенными
-                if (in_array($id, $highlightedTodos)) {
-                  // Убираем текущий todo из выделенных
-                  $newHighlightedTodos = array_diff($highlightedTodos, [$id]);
-                } else {
-                  // Добавляем текущий todo в выделенные
-                  $newHighlightedTodos[] = $id;
-                }
-
-                // Генерируем ссылку с параметрами выделенных записей
-                $params = http_build_query(['highlight' => $newHighlightedTodos]);
-                $url = $_SERVER['PHP_SELF'] . '?' . $params;
-              ?>
-              <a href="<?php echo $url; ?>" class="btn btn-outline-success btn-sm float-right">
-                <i class="fa fa-exclamation"></i>
-              </a>
-            </form>
-                <button type="button" class="btn btn-outline-danger btn-sm float-right">
-                  <i class="fa fa-trash-o"></i>
-                </button>
+                <?php if (is_array($highlightedTodos) && in_array($id, $highlightedTodos)) : ?>
+                  <span class="todo-list-item-label" style="color: steelblue; font-weight: bold">
+                  <?php else : ?>
+                    <span class="todo-list-item-label">
+                    <?php endif; ?>
+                    <?php echo $text; ?>
+                    </span>
+                    <?php if ($Statusname === 'Done') : ?>
+                      <a href="?Active=<?php echo $id; ?>" class="btn btn-outline-success btn-sm float-right">
+                        <i class="fa fa-exclamation"></i>
+                      </a>
+                    <?php else : ?>
+                      <a href="?done=<?php echo $id; ?>" class="btn btn-outline-secondary btn-sm float-right">
+                        <i class="fa fa-exclamation"></i>
+                      </a>
+                    <?php endif; ?>
+                    <a href="?delete=<?php echo $id; ?>" class="btn btn-outline-danger btn-sm float-right mr-2"><i class="fa fa-trash"></i></a>
               </td>
             </tr>
           <?php endwhile; ?>
@@ -115,4 +151,5 @@ $result = mysqli_query($link, $selectQuery);
     </div>
   </div>
 </body>
+
 </html>
